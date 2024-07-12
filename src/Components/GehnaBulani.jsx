6b04e18cdc96e44c16e42ac467e43b97179a2963
sebe <sebe2k04@gmail.com>
2024-07-12 14:39:42 +0530

    changes made

--- src/Components/GehnaBulani.jsx
+++ src/Components/GehnaBulani.jsx
@@ -61,7 +61,7 @@ const GehnaBulani = () => {
                   </h3>
                 </div>
                 <div className="">
-                  <h1 className="text-4xl font-bold">100</h1>
+                  <h1 className="text-4xl font-bold">100+</h1>
                   <h3>
                     7+ band
                     <br /> Students
